9fe0605263f74b61ed6278d34f45a1b7b4a8330e
pxeger <_@pxeger.com>
2024-03-21 22:10:55 +0000

    simplify typing

--- backend/routes/api/index.ts
+++ backend/routes/api/index.ts
@@ -1,10 +1,6 @@
 import { type Server } from "bun";
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export default async function (
-  {}: Record<string, string>,
-  _request: Request,
-  _server: Server,
-) {
+export default async function ({}, _request: Request, _server: Server) {
   return new Response("hello");
 }
